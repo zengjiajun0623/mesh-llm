5d204fbd2ca49139c826df8c38a413f8ae1c7e4e
Michael Neale <michael.neale@gmail.com>
2026-03-06 14:22:53 -0500

    v0.27.2

--- mesh-llm/src/main.rs
+++ mesh-llm/src/main.rs
@@ -14,7 +14,7 @@ use clap::{Parser, Subcommand};
 use mesh::NodeRole;
 use std::path::PathBuf;
 
-pub const VERSION: &str = "0.27.1";
+pub const VERSION: &str = "0.27.2";
 
 #[derive(Parser, Debug)]
 #[command(name = "mesh-llm", version = VERSION, about = "P2P mesh for distributed llama.cpp inference over QUIC")]
